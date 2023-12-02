e1ff4598cfd5481b9f8e181f4c45c8a4a4a2b960
Jan Korona <jan@withlocals.com>
2023-12-02 23:02:02 +0100

    day two first attempt

---
+++ spec/day_02.spec.mjs
@@ -0,0 +1,52 @@
+import { task1 } from "../lib/day_02/index.mjs";
+import data from "./input/day_02.mjs";
+
+fdescribe("Advent of code: Day 2", () => {
+
+  describe("Part 1", () => {
+
+    it("should calcuate the correct answer to example input #1", () => {
+      // given
+      const input = [
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+      ];
+
+      // when
+      const result = task1(input);
+
+      // then
+      expect(result).toEqual(8);
+    });
+
+    it("should calcuate the correct answer to example input #2", () => {
+      // given
+      const input = [
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 7 red, 2 green",
+      ];
+
+      // when
+      const result = task1(input);
+
+      // then
+      expect(result).toEqual(8);
+    });
+
+    xit("should calcuate the correct answer to ful puzzle input", () => {
+      // when
+      const result = task1(data);
+
+      // then
+      expect(result).toEqual(8);
+    });
+
+  });
+
+});
