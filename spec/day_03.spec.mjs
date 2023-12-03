ffdd3efd2c80f4eb0b2936a60e907e219e5f54cd
Jan Korona <jan@withlocals.com>
2023-12-03 22:31:07 +0100

    implemented task 2 for day 3

--- spec/day_03.spec.mjs
+++ spec/day_03.spec.mjs
@@ -1,7 +1,7 @@
-import { task1 } from "../lib/day_03/index.mjs";
+import { task1, task2 } from "../lib/day_03/index.mjs";
 import data from "./input/day_03.mjs";
 
-fdescribe("Advent of code: Day 3", () => {
+describe("Advent of code: Day 3", () => {
 
   describe("Part 1", () => {
     it("should calcuate the correct answer to example input #1", () => {
@@ -60,6 +60,38 @@ fdescribe("Advent of code: Day 3", () => {
 
       // then
       expect(result).toEqual(527369);
+    });
+  });
+
+  describe("Part 2", () => {
+    it("should calcuate the correct answer to example input", () => {
+      // given
+      const input = [
+        "467..114..",
+        "...*......",
+        "..35..633.",
+        "......#...",
+        "617*......",
+        ".....+.58.",
+        "..592.....",
+        "......755.",
+        "...$.*....",
+        ".664.598..",
+      ];
+
+      // when
+      const result = task2(input);
+
+      // then
+      expect(result).toEqual(467835);
+    });
+
+    it("should calcuate the correct answer to full puzzle input", () => {
+      // when
+      const result = task2(data);
+
+      // then
+      expect(result).toEqual(73074886);
     });
   });
 
